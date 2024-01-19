561feed9d72e7b8df9eb3510d4a168936112a3c9
MelowRainn <ruij0036@hz.nl>
2024-01-19 22:49:06 +0100

    makes it loop n drop

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -12,14 +12,12 @@ public class Main {
 
     //  allows you to read inputs in the console.
     ConsoleReader reader = new ConsoleReader();
+  
+  CalcHelper helper = new CalcHelper();
 
-    writer.writeLine("Fill in any equation");
+    writer.writeLine("Fill in any equation. Type 'exit' to quit.");
 
-    String equations[] = {
-            reader.readLine()
-    };
-
-//    examples:
+    //    examples:
 //    "ADDITION 1.0", // Error: incorrect number of values
 //    "ADDITION xx 25.0", // Error: non-numeric data
 //    "ADDITIONX 0.0 0.0", // Error: invalid command
@@ -27,11 +25,15 @@ public class Main {
 //    "ADDITION 25.0 92.0", // 25.0 + 92.0 = 117.0
 //    "SUBTRACTION 225.0 17.0", // 225.0 - 17.0 = 108.0
 //    "MULTIPLICATION 11.0 3.0" // 11.0 x 3.0 = 33.0
-    CalcHelper helper = new CalcHelper();
-    for (
-            String statements : equations) {
+    while (true) {
+      String statement = reader.readLine();
+
+      if ("exit".equalsIgnoreCase(statement)) {
+        break;
+      }
+
       try {
-        helper.process(statements);
+        helper.process(statement);
         System.out.println(helper);
       } catch (InvalidStatementException e) {
         System.out.println(e.getMessage());
