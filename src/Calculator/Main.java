ad849e62519dbecfcf98e828c22acad9be13567b
tygovanderlinde2 <Lind0133@hz.nl>
2024-01-19 21:03:28 +0100

    added a few examples for equations

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -20,11 +20,13 @@ public class Main {
     };
 
 //    examples:
-//    ADDITION
-//    SUBTRACTION
-//    MULTIPLICATION
-//    DIVISION
-
+//    "ADDING 1.0", // Error: incorrect number of values
+//    "ADDING xx 25.0", // Error: non-numeric data
+//    "ADDINGX 0.0 0.0", // Error: invalid command
+//    "DIVISION 100.0 50.0", // 100.0 / 50.0 = 2.0
+//    "ADDING 25.0 92.0", // 25.0 + 92.0 = 117.0
+//    "SUBTRACTION 225.0 17.0", // 225.0 - 17.0 = 108.0
+//    "MULTIPLICATION 11.0 3.0" // 11.0 x 3.0 = 33.0
     CalcHelper helper = new CalcHelper();
     for (
             String statements : equations) {
