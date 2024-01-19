9e658f3a99e186ff5d600c0511f517b205c57b3d
tygovanderlinde2 <Lind0133@hz.nl>
2024-01-19 20:50:38 +0100

    implemented more functions

---
+++ src/Calculator/CalcHelper.java
@@ -0,0 +1,95 @@
+package Calculator;
+
+public class CalcHelper {
+
+  private static final char ADD_SYMBOL = '+';
+  private static final char MINUS_SYMBOL = '-';
+  private static final char MULTIPLY_SYMBOL = '*';
+  private static final char DIVIDE_SYMBOL = '/';
+
+  private OpCode operation;
+  private double value1, value2, result;
+
+  public void process(String statement) throws InvalidStatementException {
+    String parts[] = statement.split(" ");
+
+    if (parts.length != 3) {
+      throw new InvalidStatementException("Incorrect number of fields", statement);
+    }
+
+    String commandString = parts[0];
+
+    try {
+      value1 = Double.parseDouble(parts[1]);
+      value2 = Double.parseDouble(parts[2]);
+    } catch (NumberFormatException e) {
+      throw new InvalidStatementException("Non-numeric data", statement, e);
+    }
+
+    setCommandFromString(commandString);
+    if (operation == null) {
+      throw new InvalidStatementException("Invalid command", statement);
+    }
+
+    CalcBase calculator = createCalculator();
+    calculator.calculate();
+    result = calculator.getResult();
+  }
+
+  private void setCommandFromString(String commandString) {
+    try {
+      operation = OpCode.valueOf(commandString.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      operation = null;
+    }
+  }
+
+  private CalcBase createCalculator() {
+    CalcBase calculator = null;
+    switch (operation) {
+      case ADDITION:
+        calculator = new CalcAdder(value1, value2);
+        break;
+      case SUBTRACTION:
+        calculator = new CalcSubtracter(value1, value2);
+        break;
+      case MULTIPLICATION:
+        calculator = new CalcSubtracter(value1, value2);
+        break;
+      case DIVISION:
+        calculator = new CalcDivider(value1, value2);
+        break;
+    }
+    return calculator;
+  }
+
+  @Override
+  public String toString() {
+    char symbol = ' ';
+    switch (operation) {
+      case ADDITION:
+        symbol = ADD_SYMBOL;
+        break;
+      case SUBTRACTION:
+        symbol = MINUS_SYMBOL;
+        break;
+      case MULTIPLICATION:
+        symbol = MULTIPLY_SYMBOL;
+        break;
+      case DIVISION:
+        symbol = DIVIDE_SYMBOL;
+        break;
+    }
+
+    StringBuilder sb = new StringBuilder(20);
+    sb.append(value1);
+    sb.append(' ');
+    sb.append(symbol);
+    sb.append(' ');
+    sb.append(value2);
+    sb.append(" = ");
+    sb.append(result);
+
+    return sb.toString();
+  }
+}
--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -3,12 +3,20 @@ package Calculator;
 import io.ConsoleReader;
 import io.ConsoleWriter;
 
+import java.io.IOException;
+
 public class Main {
-  //  allows you to write stuff to the console.
-  ConsoleWriter writer = new ConsoleWriter();
+  public static void main(String[] args) throws IOException {
+    //  allows you to write stuff to the console.
+    ConsoleWriter writer = new ConsoleWriter();
 
-  //  allows you to read inputs in the console.
-  ConsoleReader reader = new ConsoleReader();
+    //  allows you to read inputs in the console.
+    ConsoleReader reader = new ConsoleReader();
+    writer.writeLine("Fill in any equation");
+    String equations[] = {
+            reader.readLine()
+    };
+  }
 
-  String test = reader.readLine();
+
 }
